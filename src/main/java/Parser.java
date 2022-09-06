334c1955575839eec6f7dc051e52590b2282a643
Aleksandr <63749924+alexfonsid@users.noreply.github.com>
2022-09-06 14:28:26 +0300

    Parser.java some new

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -5,6 +5,8 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Parser {
 
@@ -14,20 +16,39 @@ public class Parser {
         return page;
     }
 
-    public static void main(String[] args) throws IOException {
-//        System.out.println(getPage());
-        Document page= getPage();
+//  \
+
+    private static Pattern pattern = Pattern.compile(".*\s" + "alt=" + "\\w*\\s.*");
+
+    private static String getBankFromString(String string) throws Exception {
+        Matcher matcher = pattern.matcher(string);
+        if (matcher.find()) {
+            return matcher.group();
+        }
+        throw new Exception("Can't find name of bank!");
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Document page = getPage();
         Element currencyTable = page.select("tbody[class=sort_body]").first();
-        Elements namesOfBank = currencyTable.select("tr[class=c-currency-table__main-row]");
+        Elements namesOfBank = currencyTable.select("tr[class*=c-currency-table__main-row]");
 
-//        System.out.println(currencyTable);
+        for (Element bank : namesOfBank) {
+            String nameBank = bank.getElementsByAttribute("alt").toString();
+//
+//            nameBank = bank.text();
+//            System.out.println(bank);
+//            System.out.println(nameBank);
+//            System.out.println(bank.getElementsByAttribute("alt"));
 
-        for (Element nameOfBank : namesOfBank) {
-            System.out.println(nameOfBank);
+            String name = nameBank.substring(74);
+            name = name.replaceAll("\"\s.*", "");
+            System.out.println(name);
+            String curse = bank.select("td").text();
+            System.out.println(curse);
         }
 
-        String date = "";
-//        System.out.println(date + "\t явления\t температура\t скорость\t ветра\t влажность");
-//        System.out.println("1\t        2\t        3\t        4\t        5\t        6");
+
     }
 }
